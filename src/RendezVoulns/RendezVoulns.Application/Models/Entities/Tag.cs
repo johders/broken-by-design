63fa01381a3637e2c9555410c1706246b084ef85
johders <169034808+johders@users.noreply.github.com>
2025-06-14 16:35:30 +0200

    Update tag entity class and script to be trackable

--- src/RendezVoulns/RendezVoulns.Application/Models/Entities/Tag.cs
+++ src/RendezVoulns/RendezVoulns.Application/Models/Entities/Tag.cs
@@ -1,8 +1,11 @@
+using RendezVoulns.Application.Models.Common;
+
 namespace RendezVoulns.Application.Models.Entities;
 
-public class Tag
+public class Tag : TrackableEntity
 {
     public Guid Id { get; init; } = Guid.NewGuid();
     public required string Name { get; init; }
     public required string ColorHex { get; init; } = "#cccccc";
+    public DateTimeOffset CreatedOn { get; init; } = DateTimeOffset.UtcNow;
 }
